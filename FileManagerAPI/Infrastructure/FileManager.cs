a01d8d4db7c8f6c675a3660c15d78b63a6bc9c51
Vadym Tselikin <Vadym.Tselikin@am.jll.com>
2019-03-15 16:57:53 +0200

    Update

---
+++ FileManagerAPI/Infrastructure/FileManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileManagerAPI.Infrastructure
+{
+    public class FileManager
+    {
+    }
+}
--- FileManagerAPI/Models/ChunksOfFiles.cs
+++ FileManagerAPI/Models/ChunksOfFiles.cs
@@ -12,12 +12,12 @@ namespace FileManagerAPI.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-      //  public string FileId { get; set; }
-        public string FileName { get; set; }        
-      //  public StoredFile StoredFile { get; set; }
+        public string FileId { get; set; }       
+        public string FileName { get; set; }             
         public int n { get; set; }
         public int TotalCounts { get; set; }
         public Byte[] ChunksData { get; set; }
+        
 
 
     }
