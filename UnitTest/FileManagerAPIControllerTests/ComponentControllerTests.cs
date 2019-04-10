d894c74cb5da6fc087bc9d467f505059aa2bdc6d
Vadym Tselikin <Vadym.Tselikin@am.jll.com>
2019-04-10 15:28:37 +0300

    Component Test added

--- UnitTest/FileManagerAPIControllerTests/ComponentControllerTests.cs
+++ UnitTest/FileManagerAPIControllerTests/ComponentControllerTests.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using FileManagerAPI.Controllers;
+using FileManagerBussinessLogic.Infrastructure;
+using FileManagerBussinessLogic.Interfaces;
+using FileManagerDBLogic.Models;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace UnitTest.FileManagerAPIControllerTests
 {
-    class ComponentControllerTests
+   public class ComponentControllerTests
     {
+        [Fact]
+        public async void ComponentView()
+        {
+            //arrange
+            var mock = new Mock<IFileManager>();
+            var components  = new List<StoredFile> { new StoredFile { FileName = "newFile", Size = 1234 } };
+            var componentService = new ComponentController(mock.Object);
+            //act
+            var result = await componentService.Get();
+            //assert
+            mock.Verify(c => c.GetAllFile());
+            Assert.NotEmpty(components);
+
+
+        }
     }
 }
