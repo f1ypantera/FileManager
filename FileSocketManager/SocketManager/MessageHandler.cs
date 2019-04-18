70be95ed8b73aab1b255e6e47106427137d138d8
f1ypantera <flyypaantera@gmail.com>
2019-04-18 16:03:46 +0300

    Update SocketManager

---
+++ FileSocketManager/SocketManager/MessageHandler.cs
@@ -0,0 +1,23 @@
+﻿using FileSocketManager.WebSockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSocketManager.SocketManager
+{
+    public class MessageHandler:WebSocketHandler
+    {
+        public MessageHandler(WebSocketConnectionManager webSocketConnectionManager) : base(webSocketConnectionManager)
+        {
+
+        }
+        public override async Task ReceiveAsync(WebSocket socket, WebSocketReceiveResult result, byte[] buffer)
+        {
+            string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+            await SendMessageToAllAsync(message);
+        }
+    }
+}
